4c76f69fe5a02d978cc227de24ce2dd02c024f26
Shubham <ashutosh51198@gmail.com>
2023-07-29 20:25:40 +0530

    styles added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+
 
 function App() {
+
+    const title='Feedback-App'
+    const body='this is First Feedback For the website';
+
+    const loading =false;
+    const showcomments=true;
+    if(loading) return <h1>Loading...</h1>
+
+   
+
+    const comments=[
+        {id:1,text:'Comment One'},
+        {id:2,text:'Comment two'},
+        {id:3,text:'Comment Three'}
+    ]
+    
+
+    const commentBlock=( <div className='comments'>
+    <h3> Comments({comments.length})</h3>
+
+    <ul>
+        {comments.map((comment,index)=>(
+            <li key={index}>{comment.text}</li>
+
+        ))}
+    </ul>
+</div>)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Header text='Feedback-App'/>
+    <div className="container">
+      <h1>My App</h1>
+      <p>hi this is feedback-app</p>
+      {showcomments && commentBlock} 
     </div>
+    </>
   );
 }
-
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header({text}) {
+  return (
+    <header>
+      <div className="container" >
+        <h1>{text}</h1>
+      </div>
+    </header>
+  )
+}
+
+export default Header
